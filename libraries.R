6187b0de9033668a812f2db55cf87008b71b579c
Florian Wisniewski <f.wisniewski@web.de>
2021-02-24 17:29:20 +0100

    24-02-2021: added some descriptions - added filters for minority governments - begun listing vars for full model (in acc. to Saalfeld 2008)

--- libraries.R
+++ libraries.R
@@ -12,6 +12,7 @@ library(lubridate)
 library(Amelia)
 library(xtable)
 library(haven)
+library(foreign)
 library(broom)
 library(psych)
 library(Hmisc)
@@ -95,26 +96,111 @@ summary(coxph(data = dtaBerg, firstSurv ~ minor_coalition))
 
 
 
-########################################### filter for minority cabinets
-# var for minority coalition cabinet - minor_coalition
+########################################### filter df for relevant vars
+# vars for minority coalition cabinet - minor_coalition and one_party_minor
 fre(minor_coalition)
 ## in this case 0 == no; 1 == yes
 ## there are 113 minority cabinets in the df
 
+fre(one_party_minor)
+## coding equal to minor_coalition
 
-# filter df for minor_coalition
-filterBerg <- filter(dtaBerg, minor_coalition == 1)
+# var for post election cabinet
+fre(post_election_cabinet)
+## study to include only those governments that came to power through regular elections
+## at the end of respective national legislative periods
+## codes as 1 == yes; 0 == no
+
+# filter df for minority coalitions (one_party_minor | minor_coalition)
+filterBerg <- filter(dtaBerg, post_election_cabinet == 1 & minor_coalition == 1 | one_party_minor == 1)
 View(filterBerg)
+save(filterBerg, file = "filterBergmann.RData")
+## the df is filtered for post election cabinets == T AND minority coalition governments OR
+## one party minority governments == T
+
 
 # new Surv-item with filterBerg
 Surv(filterBerg$abs_dur, event = filterBerg$discr2019)
 
 
 # fit test model for this df
-summary(coxph(data = filterBerg, Surv(filterBerg$abs_dur, event = filterBerg$discr2019) ~ filterBerg$rl_range + filterBerg$bicameralism + filterBerg$strong_2chamber + filterBerg$gov_comp))
+testModel <- coxph(data = filterBerg, Surv(filterBerg$abs_dur, event = filterBerg$discr2019) ~ filterBerg$eff_numb_parties + filterBerg$cab_barg_duration)
+summary(testModel)
+## coef here is showing hazard RATIOS
+
+# test: proportional hazards assumption met?
+cox.zph(testModel)
+ggcoxzph(cox.zph(testModel))
+## when significant values at vars or on global level: PH assumption is NOT met! Has to be insignificant!
+## graphically: Schoenfeld test - there must not be a pattern of shifting values at any given point in time!
 
 
+###########################################  vars to include?
+# dependent var and miscellaneous/other
 
+## discr2019 --- discretionary cabinet terminations (2019)
+### code: 1 == discretionary ; 0 == others
+
+## for event history analysis/data over time: abs_dur --- absolute duration (days)
+
+## filtered vars are one_party_minor and minor_coalition (filter for minority cabinets only)
+## and post_election_cabinet (cabinets after elections at the end of legislative periods)
+
+# independent vars (based on Saalfeld 2008)
+
+## eff_numb_parties --- effective number of parties
+### based on Laakso/Taagepera index (1979)
+
+## cab_barg_duration --- cabinet bargaining duration
+### measured in days
+
+## pm_diss_pow --- Prime Minister dissolution powers
+### index from 0-10 based on Goplerud/Schleiter 2016
+### additionally: look up what Saalfeld was using in 2008
+
+## rl-range --- overall parliamentary RILE range
+### range = maximum - minimum; higher values = more RILE polarization
+### this is for the parliament as a whole - is another polarization var needed just for the cab?
+#### possibility to merge ERDDA variable for this into dtaBerg?
+
+## polarization_bpw --- polarization (bp weighted)
+### polarization of cabinets, value as calculated in ERDDA 2014, hence also only data for countries from ERDDA
+### rather include this for cabinet preference range?
+
+## 
+
+## building a subset with said vars
+detach(dtaBerg)
+attach(filterBerg)
+subset(data = filterBerg, select = c(discr2019, abs_dur, one_party_minor, minor_coalition, 
+                                     post_election_cabinet, eff_numb_parties, cab_barg_duration,
+                                     pm_diss_pow, polarization_bpw, rl_range))
+
+
+###########################################  merge dfs for some reasons?
+
+# include cabinet preference range from ERDDA into dtaBergmann
+
+## find proper function for merging - merge or cbind?
+### cannot use cbind because structure is not entirely the same! This would cause the values to end up
+### in wrong locations - therefore merge has to be used
+
+# get some overview
+head(dtaBerg)
+head(erdda)
+all_equal(dtaBerg, erdda)
+### this just shows different number of columns (158 vs 202) - is the rest the same df?
+
+# id vars are erdda$v001e and dtaBerg
+# try merging by "country"
+help(merge)
+merge(dtaBerg, erdda, by.x = "country", by.y = "v001e")
+merge(dtaBerg, erdda, by = NULL)
+
+BergErdda <- merge(dtaBerg, erdda, by = NULL)
+fre(BergErdda$v405e)
+
+### this does not really work (apparently...): ERDDA values are not taken to dtaBerg
 
 
 
